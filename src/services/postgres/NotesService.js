4892f5a0d15a0d5cdbeb7f2d279a88231c1c8d1b
Ivan <ivanwinata1972@gmail.com>
2025-11-25 02:50:54 +0800

    authorization implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,19 +1,28 @@
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
+const { mapDBToModel } = require('../../utils'); // jika tidak punya, hapus baris ini
+
 
 class NotesService {
   constructor() {
     this._notes = [];
   }
 
-  addNote({ title, body, tags }) {
+  addNote({ title, body, tags, owner }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
     const newNote = {
-      title, tags, body, id, createdAt, updatedAt,
+      id,
+      title,
+      body,
+      tags,
+      createdAt,
+      updatedAt,
+      owner,
     };
 
     this._notes.push(newNote);
@@ -21,21 +30,24 @@ class NotesService {
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
     if (!isSuccess) {
-      throw new InvariantError('Catatan gagal ditambahkan');;
+      throw new InvariantError("Catatan gagal ditambahkan");
     }
 
     return id;
   }
 
-  getNotes() {
-    return this._notes;
+  getNotes(owner) {
+    const notes = this._notes.filter((note) => note.owner === owner);
+    return notes;
   }
 
   getNoteById(id) {
-    const note = this._notes.filter((n) => n.id === id)[0];
+    const note = this._notes.find((n) => n.id === id);
+
     if (!note) {
-      throw new NotFoundError('Catatan tidak ditemukan');;
+      throw new NotFoundError("Catatan tidak ditemukan");
     }
+
     return note;
   }
 
@@ -43,7 +55,7 @@ class NotesService {
     const index = this._notes.findIndex((note) => note.id === id);
 
     if (index === -1) {
-      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');;
+      throw new NotFoundError("Gagal memperbarui catatan. Id tidak ditemukan");
     }
 
     const updatedAt = new Date().toISOString();
@@ -51,18 +63,31 @@ class NotesService {
     this._notes[index] = {
       ...this._notes[index],
       title,
-      tags,
       body,
+      tags,
       updatedAt,
     };
   }
 
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
+
     if (index === -1) {
-      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');;
+      throw new NotFoundError("Catatan gagal dihapus. Id tidak ditemukan");
     }
+
     this._notes.splice(index, 1);
+  }
+  async verifyNoteOwner(id, owner) {
+    const note = this._notes.find((n) => n.id === id);
+
+    if (!note) {
+      throw new NotFoundError("Resource yang Anda minta tidak ditemukan");
+    }
+
+    if (note.owner !== owner) {
+      throw new AuthorizationError("Anda tidak berhak mengakses resource ini");
+    }
   }
 }
 
